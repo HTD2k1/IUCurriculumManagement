4720e2528edfa125a0b89bccdc0328b12f2e2a45
HA TIEN DAT <ITITIU19095@student.hcmiu.edu.vn>
2023-03-30 00:58:16 +0700

    add Mediator project and UploadToBlogService

---
+++ CurriculumMeditator/Program.cs
@@ -0,0 +1,54 @@
+using Microsoft.AspNetCore.Mvc;
+
+namespace CurriculumMeditator
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var builder = WebApplication.CreateBuilder(args);
+
+            // Add services to the container.
+            builder.Services.AddAuthorization();
+
+            // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+            builder.Services.AddEndpointsApiExplorer();
+            builder.Services.AddSwaggerGen();
+
+            var app = builder.Build();
+
+            // Configure the HTTP request pipeline.
+            if (app.Environment.IsDevelopment())
+            {
+                app.UseSwagger();
+                app.UseSwaggerUI();
+            }
+
+            app.UseHttpsRedirection();
+
+            app.UseAuthorization();
+
+            var summaries = new[]
+            {
+            "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+        };
+
+            app.MapPost("/create-currriculum",  async (HttpContext httpContext) =>
+            {
+                var formFile = httpContext.Request.Form.Files.FirstOrDefault();
+
+                if (formFile == null || formFile.Length == 0)
+                {
+                    return Results.BadRequest();
+                }
+
+                await BlobStorageService.UploadFiles(formFile);
+                RabbitMQPublishClient.Publish($"{formFile.FileName}");
+                return Results.Ok();
+            })
+            .WithName("Post");
+
+            app.Run();
+        }
+    }
+}
---
+++ CurriculumMeditator/RabbitMQPublishClient.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Connections;
+using RabbitMQ.Client;
+using System.Text;
+
+namespace CurriculumMeditator
+{
+    public class RabbitMQPublishClient
+    {
+       public static void Publish(string message)
+        {
+            var factory = new ConnectionFactory { HostName = "localhost" };
+            using var connection = factory.CreateConnection();
+            using var channel = connection.CreateModel();
+            
+            channel.BasicPublish(exchange: string.Empty,
+                                                    routingKey: "curriculum-created",
+                                                    basicProperties: null,
+                                                    body: Encoding.UTF8.GetBytes(message));
+        }
+    }
+}
