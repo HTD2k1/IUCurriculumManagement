8394c6da01f19d8295052304a339211921f9f37a
HTD2k1 <58422272+HTD2k1@users.noreply.github.com>
2023-04-12 19:12:29 +0700

    add BlobStorageService interface

---
+++ BuildingBlocks/BlogStorageService/IBlobStorageService.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Http;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BlogStorageService
+{
+    public interface IBlobStorageService
+    {
+        public Task UploadFiles(IFormFile file);
+        public Task DownloadFiles(IFormFile file);
+    }
+}
